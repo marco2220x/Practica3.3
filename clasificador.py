05281abcae463d5a0e0d510496d59fcce7508caa
Eeveeelyyyn <evsolacopernico@hotmail.com>
2023-12-08 10:57:47 -0600

    Clasificador Changes

--- clasificador.py
+++ clasificador.py
@@ -1,1 +1,28 @@
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
+class NaiveBayesGaussiano:
+    def __init__(self):
+        self.medias = None
+        self.desviaciones = None
+        self.probabilidades_clases = None
+
+    def fit(self, X, y):
+        # Utilizar LabelEncoder para convertir etiquetas de cadena a números
+        self.label_encoder = LabelEncoder()
+        y_numerico = self.label_encoder.fit_transform(y)
+
+        clases = np.unique(y_numerico)
+        num_clases = len(clases)
+        num_caracteristicas = X.shape[1]
+
+        # Calcular medias y desviaciones estándar para cada clase y característica
+        self.medias = np.zeros((num_clases, num_caracteristicas))
+        self.desviaciones = np.zeros((num_clases, num_caracteristicas))
+        self.probabilidades_clases = np.zeros(num_clases)
+
+        for i, clase in enumerate(clases):
+            datos_clase = X[y_numerico == clase]
+            self.medias[i] = np.mean(datos_clase, axis=0)
+            self.desviaciones[i] = np.std(datos_clase, axis=0)
+            self.probabilidades_clases[i] = len(datos_clase) / len(y_numerico)
