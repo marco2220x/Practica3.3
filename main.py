ae9b9bfb2d1c50d23fd866712b2f84182a851df9
AranzaMich <aranzamichelle1@hotmail.com>
2023-12-08 11:15:02 -0600

    cambios main

--- main.py
+++ main.py
@@ -24,3 +24,46 @@ if __name__ == "__main__":
 
                 imagenes.append(imagen)
                 etiquetas.append(subcarpeta)
+                caracteristicas = extraer_caracteristicas(imagenes)
+
+    # Dividir los datos en conjuntos de entrenamiento y pruebas de manera estratificada
+    X_train, X_test, y_train, y_test = train_test_split(caracteristicas, etiquetas, test_size=0.25, stratify=etiquetas, random_state=42)
+
+    '''Clasificador Manual'''
+    # Crear el clasificador gaussiano ingenuo
+    clasificador_manual = NaiveBayesGaussiano()
+
+    # Entrenar el clasificador con el conjunto de entrenamiento
+    clasificador_manual.fit(X_train, y_train)
+
+    # Realizar predicciones en el conjunto de pruebas
+    predicciones_manual = clasificador_manual.predict(X_test)
+
+    print("Precisión (manual):", accuracy_score(y_test, predicciones_manual))
+    print("Matriz de Confusión (manual):")
+    print(confusion_matrix(y_test, predicciones_manual))
+    print("Reporte de Clasificación (manual):")
+    print(classification_report(y_test, predicciones_manual))
+
+
+    '''Clasificador con la biblioteca'''
+    # Crear el clasificador gaussiano ingenuo
+    clasificador = GaussianNB()
+
+    # Entrenar el clasificador con el conjunto de entrenamiento
+    clasificador.fit(X_train, y_train)
+
+    # Realizar predicciones en el conjunto de pruebas
+    predicciones = clasificador.predict(X_test)
+
+
+    precisión = accuracy_score(y_test, predicciones)
+    print(f"Precisión: {precisión}")
+    matriz_confusion = confusion_matrix(y_test, predicciones)
+    print("Matriz de Confusión:")
+    print(matriz_confusion)
+    reporte_clasificacion = classification_report(y_test, predicciones)
+    print("Reporte de Clasificación:")
+    print(reporte_clasificacion)
+
+                  
