da5150e26673abc0866dbb955bacbaca12ab1cf2
Daniel Vega <vegad8510@gmail.com>
2023-12-08 11:08:32 -0600

    Función extraer características

--- caracteristicas.py
+++ caracteristicas.py
@@ -1,1 +1,11 @@
+from skimage.feature import hog
+import numpy as np
+
+def extraer_caracteristicas(imagenes):
+    caracteristicas = []
+    for imagen in imagenes:
+        caracteristica_hog = hog(imagen, orientations=8, pixels_per_cell=(16, 16),
+                                 cells_per_block=(1, 1), block_norm='L2-Hys')
+        caracteristicas.append(caracteristica_hog)
+    return np.array(caracteristicas)
 
